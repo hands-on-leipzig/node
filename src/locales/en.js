0860abc6c6cd28737383ae3b1f6aca9146da03ad
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2026-03-20 09:54:39 +0100

    i18n: update de + en translations (@sv@hands-on-technology.org) (#6)
    * chore(i18n): update en locale
    
    * chore(i18n): update de locale

--- src/locales/en.js
+++ src/locales/en.js
@@ -222,7 +222,7 @@ export default {
         "mockEventLandesfinale": "State final Saxony",
         "mockEventLandesfinaleDate": "May 2026",
         "mockEventLandesfinaleLocation": "Dresden",
-        "intro": "Register teams, classes or groups.",
+        "intro": "Here you can register your team or class for the FIRST LEGO League.",
         "enrollTeam": "Register a team",
         "enrollTeamDesc": "Register a team for the competition.",
         "enrollClass": "Register a class",
